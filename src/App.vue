f4110201d943952ae7ea335f54e21e007d85a304
61130061 <thezeroyou@gmail.com>
2022-10-12 00:11:02 +0700

    🚀 add cheat sheet JSON data

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import Navbar from "./components/Navbar.vue";
 import Item from "./components/Item.vue";
 
 import layout from "./data/layout.json";
+import flexAndGrid from "./data/FnG.json";
+import spacing from "./data/Spacing.json";
+import sizing from "./data/Sizing.json";
+import typography from "./data/Typography.json";
+import backgrounds from "./data/Backgrounds.json";
 
 export default {
   components: {
@@ -11,7 +16,12 @@ export default {
   },
   data() {
     return {
-      layout
+      layout,
+      flexAndGrid,
+      spacing,
+      sizing,
+      typography,
+      backgrounds
     }
   }
 }
@@ -21,11 +31,32 @@ export default {
   <div class="h-screen flex flex-col dark:bg-gray-900 text-gray-800 dark:text-gray-50">
     <Navbar />
     <div class="flex-1">
-      <div class="flex m-5">
-        <div class="bg-gray-200 p-2 rounded-lg w-[350px]">
+      <div class="m-5 flex flex-wrap gap-5">
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
           <div class="px-3 py-2 mb-2 font-bold">Layout</div>
           <Item v-for="(data, index) in layout" :data="data" />
         </div>
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
+          <div class="px-3 py-2 mb-2 font-bold">Flexbox and Grid</div>
+          <Item v-for="(data, index) in flexAndGrid" :data="data" />
+        </div>
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
+          <div class="px-3 py-2 mb-2 font-bold">Spacing</div>
+          <Item v-for="(data, index) in spacing" :data="data" />
+        </div>
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
+          <div class="px-3 py-2 mb-2 font-bold">Sizing</div>
+          <Item v-for="(data, index) in sizing" :data="data" />
+        </div>
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
+          <div class="px-3 py-2 mb-2 font-bold">Typography</div>
+          <Item v-for="(data, index) in typography" :data="data" />
+        </div>
+        <div class="bg-gray-200 p-2 rounded-lg w-[350px] h-fit">
+          <div class="px-3 py-2 mb-2 font-bold">Backgrounds</div>
+          <Item v-for="(data, index) in backgrounds" :data="data" />
+        </div>
+
       </div>
     </div>
   </div>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,12 @@
 <script lang="ts">
   const getColor = (name) => {
     let className = ['py-2']
-    if (name === 'class') className.push('text-blue-500')
-    else if (name === 'property') className.push('text-purple-500')
+    if (name.toLowerCase() === 'class') className.push('text-blue-500')
+    else if (name.toLowerCase() === 'properties') className.push('text-purple-500')
     else className.push('text-gray-500')
+
+    if (name.toLowerCase() !== 'class') className.push('break-words')
+
     return className.join(' ');
   }
 
@@ -28,15 +31,15 @@
 <template>
   <div class="mb-3">
     <div @click="onToggle" class="p-3 bg-white rounded drop-shadow font-[600] hover:cursor-pointer">{{data.name}}</div>
-    <div v-if="showInfo" class="bg-gray-100 rounded-lg px-3 m-2 my-3">
+    <div v-if="showInfo" class="bg-gray-100 rounded-lg px-3 m-2">
       <table class="table-auto text-left w-full">
         <thead class="border-b-2">
           <tr>
-            <th v-for="(v, k) in data.classes[0]" class="py-3">{{k[0].toUpperCase() + k.substring(1)}}</th>
+            <th v-for="(v, k) in data.table[0]" class="py-3">{{k}}</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(c) in data.classes">
+          <tr v-for="(c) in data.table">
             <td v-for="(v, k) in c" v-bind:class="getColor(k)">{{v}}</td>
           </tr>
         </tbody>
