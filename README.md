e656cf63dc48cd5064684d7a14ec18b427c98257
61130061 <thezeroyou@gmail.com>
2022-10-11 13:48:00 +0700

    ➕ initail vue.js + tailwind commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind Cheat Sheet
+
+Building fast, simple, and ready-to-use cheat sheet website for Tailwind CSS user.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen flex dark:bg-gray-900 text-gray-800 dark:text-gray-50">
+    <div class="m-auto">
+      <h1 class="text-6xl font-[800]">Tailwind <span class="text-blue-400">Cheat Sheet</span></h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
